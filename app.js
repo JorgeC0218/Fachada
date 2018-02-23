5155fcc2e36f5dc9d0cbc0e6b13a7a8b075a3eaf
marcocastillo2506 <marcocastillo2506@gmail.com>
2018-02-23 16:39:38 -0600

    Structured authentication

--- app.js
+++ app.js
@@ -1,81 +1,72 @@
 var express = require('express');
 var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-var exphbs = require('express-handlebars');
-var expressValidator = require('express-validator');
-var flash = require('flash');
-var session = require('express-session');
-var passport = require('passport');
-var LocalStrategy = require('passport-local');
-var LocalStrategy = require('Strategy');
-var mongo = require('mongodb');
-var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost//Fachada');
-var db = mongoose.connection() ;﻿
 
-var routes = require ('./routes/index');
-var users = require ('./routes/users');
+var index = require('./routes/index');
+var users = require('./routes/users');
 
-//init app
 var app = express();
 
-////view Engine
-app.set('views',path.join(__dirname,"views"));
-app.engine('handlebars',exphbs({defaultLayout:'layout'}));
-app.set('view engine','handlebars');
+require('dotenv').config();
 
-//bodyParser Middleware
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
 
-//set static folder
-app.use(express.static(path.join(__dirname,'public')));
+app.use('/', index);
+app.use('/users', users);
 
-//Express session
-app.use(session({
-  secret:'secret',
-  saveUnitialized: true,
-  resave: true
-}));
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
 
-//passport init
-app.use(passport.initialize());
-app.use(passport.session());
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-//Express validator
-app.use(expressValidator({
-  errorFormatter: function(param, msg, value){
-    var namespace = param.split('.'),
-    root = namespace.shift(),
-    formParam = root;
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
 
-    while(namespace.length){
-      formParam += '[' + namespace.shift() +']';
-    }
-    return {
-      param : formParam,
-      msg   : msg,
-      value : value
-    };
+
+// Handlebars default config
+const hbs = require('hbs');
+const fs = require('fs');
+
+const partialsDir = __dirname + '/views/partials';
+
+const filenames = fs.readdirSync(partialsDir);
+
+filenames.forEach(function (filename) {
+  const matches = /^([^.]+).hbs$/.exec(filename);
+  if (!matches) {
+    return;
   }
-}));
-
-//connect flash
-app.use(flash());
-
-//Global vars
-app.use(function(req, res,  next){
-  res.locals.success_msg = req.flash('success_msg');
-  res.locals.error_msg = req.flash('error_msg');
-  res.locals.error = req.flash('error');
-  next();
+  const name = matches[1];
+  const template = fs.readFileSync(partialsDir + '/' + filename, 'utf8');
+  hbs.registerPartial(name, template);
 });
 
-//Set port
-app.set('port', (process.env.PORT || 3000));
-
-app.listen(app.get('port'), function (){
-  console.log("Server started on port" + app.get('port'));
+hbs.registerHelper('json', function(context) {
+    return JSON.stringify(context, null, 2);
 });
+
+
+module.exports = app;
